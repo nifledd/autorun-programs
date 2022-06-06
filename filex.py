3208a48472cca980997098b4e33aef2b350c2608
Damian Ch <dcha.projekty@gmail.com>
2022-06-06 22:45:16 +0200

    Dodanie progressbaru

---
+++ filex.py
@@ -0,0 +1,34 @@
+import os
+import time
+def printProgressBar (iteration, total, prefix = '', suffix = '', decimals = 1, length = 100, fill = '█', printEnd = "\r"):
+    """
+    Call in a loop to create terminal progress bar
+    @params:
+        iteration   - Required  : current iteration (Int)
+        total       - Required  : total iterations (Int)
+        prefix      - Optional  : prefix string (Str)
+        suffix      - Optional  : suffix string (Str)
+        decimals    - Optional  : positive number of decimals in percent complete (Int)
+        length      - Optional  : character length of bar (Int)
+        fill        - Optional  : bar fill character (Str)
+        printEnd    - Optional  : end character (e.g. "\r", "\r\n") (Str)
+    """
+    percent = ("{0:." + str(decimals) + "f}").format(100 * (iteration / float(total)))
+    filledLength = int(length * iteration // total)
+    bar = fill * filledLength + '-' * (length - filledLength)
+    print(f'\r{prefix} |{bar}| {percent}% {suffix}', end = printEnd)
+    # Print New Line on Complete
+    if iteration == total:
+        print()
+allFiles = os.listdir("exec")
+
+
+items = list(range(len(allFiles)))
+l = len(allFiles)
+
+i = 0
+for file in allFiles:
+    os.startfile(f"exec\\{file}")
+    printProgressBar(i + 1, l, prefix='Progress:', suffix='Complete', length=50)
+    i += 1
+input()
